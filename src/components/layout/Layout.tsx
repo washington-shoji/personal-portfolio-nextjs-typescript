72c7019dc04d7e63c1dae5a7ed324353ef119af3
washington-shoji <shoji.washington@gmail.com>
2022-01-27 20:54:34 +1100

    refactor(component): Refactored the layout component and styling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,17 @@
 import {ScriptProps} from 'next/script';
 
 import styles from '../../../styles/Layout.module.scss';
+import Footer from '../footer/Footer';
 import NavigationBar from '../navigation-bar/NavigationBar';
 
 export function Layout({children}: ScriptProps): JSX.Element {
 	return (
-		<div className={(styles.layout_container, styles.gradient__background)}>
+		<div
+			className={`${styles.layout_container} ${styles.gradient__background}`}
+		>
 			<NavigationBar />
 			<div>{children}</div>
-			<div>
-				<h1>Footer</h1>
-			</div>
+			<Footer />
 		</div>
 	);
 }
