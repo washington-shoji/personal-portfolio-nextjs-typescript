e161c78c7b03807af91f27f566dc06a36f9a4edd
washington-shoji <shoji.washington@gmail.com>
2022-01-28 16:13:14 +1100

    refactor(components): Refactored the experience component and styling

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -4,22 +4,22 @@ const experienceData = [
 	{
 		id: 1,
 		title: 'Freelancing working',
-		text: 'I have worked on different projects, and experienced several tech stacks.',
+		text: 'As a freelance developer, I develop websites and apps to improve the digital performance of small businesses and not-for-profit organisations. I work closely with stakeholders to understand and create solutions for their needs, using a tech stack that is sustainable for their use case.',
 	},
 	{
 		id: 2,
 		title: 'Business Analyst',
-		text: 'I have worked on different projects, and experienced several tech stacks.',
+		text: 'Before becoming a full-stack web developer, I worked for The NSW Department of Education. The department is responsible for governing over 2,200 public schools and education programs in the state. My role here was to conduct analyses of internal data sources, to determine how to best allocate available resources to have maximum impact on the schools.',
 	},
 	{
 		id: 3,
 		title: 'Master of IT',
-		text: 'I have worked on different projects, and experienced several tech stacks.',
+		text: 'Graduated in 2017 - University Technology Sydney',
 	},
 	{
 		id: 4,
 		title: 'Bachelor of Commerce',
-		text: 'I have worked on different projects, and experienced several tech stacks.',
+		text: 'Graduated in 2010 - Macquarie University',
 	},
 ];
 
