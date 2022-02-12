8273743aec71532f867d73bfa1022b70becae7e6
washington-shoji <shoji.washington@gmail.com>
2022-02-12 21:24:37 +1100

    refactor(page): Refactored the resources page to pass static props

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,45 +1,36 @@
 import { NextPage } from 'next';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../styles/Resources.module.scss';
-import bgImage from '../public/assets/images/bg_construction_2.jpg';
-import Image from 'next/image';
+
 import ResourceCard from '../src/components/resources/ResourceCard';
+import {
+	IResourcesPost,
+	resourcePosts,
+} from '../src/resources-data/resources-data';
 
-const resourceCardContent = [
-	{
-		title: 'Title 1',
-	},
-	{
-		title: 'Title  2',
-	},
-	{
-		title: 'Title 3',
-	},
-	{
-		title: 'Title  4',
-	},
-	{
-		title: 'Title 5',
-	},
-	{
-		title: 'Title 6',
-	},
-];
+export async function getStaticProps() {
+	const posts: IResourcesPost[] = resourcePosts;
 
-export default function Resources<NextPage>(): JSX.Element {
+	return {
+		props: { posts },
+	};
+}
+
+const Resources: NextPage = ({ posts }: any) => {
 	return (
 		<div className={styles.container}>
-			{/* <Image src={bgImage} alt='' /> */}
 			<div className={styles.header}>
 				<h1>Resources</h1>
 				<p>This is a repo of resources dedicated to the lovely community.</p>
 				<p>{`I hope it's useful for you.`}</p>
 			</div>
 			<div className={styles.body}>
-				{resourceCardContent.map((content) => {
-					return <ResourceCard key={content.title} {...content} />;
+				{posts.map((content: IResourcesPost) => {
+					return <ResourceCard key={content.Title} {...content} />;
 				})}
 			</div>
 		</div>
 	);
-}
+};
+
+export default Resources;
