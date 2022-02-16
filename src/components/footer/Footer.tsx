cf148a257f4c17bce99bc908a8abe5448ecaf8ff
washington-shoji <shoji.washington@gmail.com>
2022-02-16 15:05:14 +1100

    refactor(component): Refactored the footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,25 @@
+import Link from 'next/link';
 import { FaSignature } from 'react-icons/fa';
-import styles from '../../../styles/Footer.module.scss';
+import { navData } from '../navigation-bar/NavigationBar';
+import styles from './Footer.module.scss';
 
 export default function Footer(): JSX.Element {
 	return (
 		<footer className={styles.container}>
-			<div className={styles.footer_brand}>
-				<div className={styles.footer_logo}>{<FaSignature />}</div>
-				<p>Washington Shoji</p>
-			</div>
-			<div className={styles.footer_links}>
-				<p>Links</p>
-				<p>Home</p>
-				<p>Skills</p>
-				<p>Projects</p>
-				<p>Experience</p>
-				<p>Resources</p>
-			</div>
-			<div className={styles.footer_links}>
-				{/* <p>Links</p>
-				<p>Privacy</p>
-				<p>T&C</p> */}
+			<div>
+				<nav className={styles.footer_nav}>
+					<div className={styles.footer_brand}>
+						<div className={styles.footer_logo}>{<FaSignature />}</div>
+					</div>
+					{navData.map((item) => {
+						return (
+							<Link key={item.title} href={`${item.link}`}>
+								<a className={styles.footer__link}>{item.title}</a>
+							</Link>
+						);
+					})}
+					<div className={styles.animation}></div>
+				</nav>
 			</div>
 
 			<div className={styles.footer_copyright}>
