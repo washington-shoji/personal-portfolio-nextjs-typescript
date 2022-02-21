5d65e0c7d92997f24981b07f5bd75a9fd2706bf3
washington-shoji <shoji.washington@gmail.com>
2022-02-21 16:34:14 +1100

    feat(component): Added a slider component

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { projectData } from '../../resources-data/project-data';
+import Card from '../card/Card';
+import styles from './Slider.module.scss';
+import { IProjectData } from './../../resources-data/project-data';
+
+export default function Slider({ slides }: { slides: IProjectData[] }) {
+	const [current, setCurrent] = useState(0);
+	const length = slides.length;
+
+	const nextSlide = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1);
+	};
+
+	const prevSlide = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1);
+	};
+
+	if (!Array.isArray(slides) || slides.length <= 0) {
+		return null;
+	}
+	return (
+		<div className={styles.container}>
+			<div className={styles.slider}>
+				<div className={styles.left__arrow} onClick={prevSlide}>
+					<span className={styles.border}>{`<`}</span>
+				</div>
+				<div className={styles.right__arrow} onClick={nextSlide}>
+					<span className={styles.border}>{`>`}</span>
+				</div>
+				{projectData.map((slide: any, index: number) => {
+					return (
+						<div
+							className={
+								index === current
+									? `${styles.slide} ${styles.active}`
+									: `${styles.slide}`
+							}
+							key={index}
+						>
+							{index === current && (
+								<div className={styles.slider__content}>
+									<Card key={slide.id} data={slide} />
+								</div>
+							)}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
