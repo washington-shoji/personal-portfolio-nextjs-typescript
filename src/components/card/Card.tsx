02028cf03bb9d75bd0f499b81f9fe00e951b7444
washington-shoji <shoji.washington@gmail.com>
2022-02-22 17:07:04 +1100

    refactor(component): Refactored the image in the card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { IProjectData } from '../../resources-data/project-data';
 import Button from '../button-exp/Button';
 import styles from './Card.module.scss';
+import Image from 'next/image';
 
 export default function Card({ data }: { data: IProjectData }) {
 	const { id, title, image, description, liveLink, githubLink } = data;
 	return (
 		<div className={styles.container}>
 			<h1>{title}</h1>
-			<img src={image} alt='' />
+			<Image
+				className={styles.project_image}
+				src={data.image}
+				alt=''
+				width={450}
+				height={450}
+			/>
+			{/* <img src={image} alt='' /> */}
 			<div className={styles.content}>
 				<h3>More Info</h3>
 				<p>{description}</p>
