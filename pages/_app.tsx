adefcf3067037100b9f6b4f0a9571d275ea213f4
washington-shoji <shoji.washington@gmail.com>
2022-02-22 17:48:36 +1100

    refactor(minor): Minor refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Layout } from '../src/components/layout/Layout';
-import { LayoutExp } from '../src/components/layout_exp/LayoutExp';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
-		<LayoutExp>
+		<Layout>
 			<Component {...pageProps} />
-		</LayoutExp>
-		// <Layout>
-		// 	<Component {...pageProps} />
-		// </Layout>
+		</Layout>
 	);
 }
 
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import { IProjectData } from '../../resources-data/project-data';
-import Button from '../button-exp/Button';
+import { IProjectsData } from '../../resources-data/project-data';
+import Button from '../button/Button';
 import styles from './Card.module.scss';
 import Image from 'next/image';
 
-export default function Card({ data }: { data: IProjectData }) {
-	const { id, title, image, description, liveLink, githubLink } = data;
+export default function Card({ data }: { data: IProjectsData }) {
+	const { title, image, description, demoLink, githubLink } = data;
 	return (
 		<div className={styles.container}>
 			<h1>{title}</h1>
 			<Image
 				className={styles.project_image}
-				src={data.image}
+				src={image}
 				alt=''
 				width={450}
 				height={450}
 			/>
-			{/* <img src={image} alt='' /> */}
 			<div className={styles.content}>
 				<h3>More Info</h3>
 				<p>{description}</p>
 				<div className={styles.button}>
-					<Button externalLink={liveLink} title={'Live demo'} />
+					<Button externalLink={demoLink} title={'Live demo'} />
 					<Button internalLink={githubLink} title={'GitHub'} />
-					{/* <button>More Info</button>
-					<button>More Info</button> */}
 				</div>
 			</div>
 		</div>
