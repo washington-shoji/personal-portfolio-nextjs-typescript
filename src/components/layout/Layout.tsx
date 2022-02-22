f94e3da7b0a6d48404ef7d6bbf47bca9793b477b
washington-shoji <shoji.washington@gmail.com>
2022-02-22 17:50:47 +1100

    refactor(component): Refactored the layout component and styling to fit the new design

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,50 +1,11 @@
 import { ScriptProps } from 'next/script';
-import { useCallback, useEffect, useState } from 'react';
-
+import React from 'react';
 import styles from './Layout.module.scss';
-import Footer from '../footer/Footer';
-import MobileMenu from '../mobile-menu/MobileMenu';
-import NavigationBar from '../navigation-bar/NavigationBar';
 
 export function Layout({ children }: ScriptProps): JSX.Element {
-	const [open, setOpen] = useState<boolean>(false);
-
-	function handleClick(): void {
-		setOpen(!open);
-	}
-
-	const hiddenWrapper = useCallback(() => {
-		function hideScrolling() {
-			if (open === true) {
-				document.body.style.overflow = 'hidden';
-			} else {
-				document.body.style.overflow = '';
-				document.body.style.overflow = 'scroll';
-			}
-		}
-		hideScrolling();
-	}, [open]);
-
-	useEffect(() => {
-		hiddenWrapper();
-	}, [hiddenWrapper, open]);
-
 	return (
-		<div>
-			{open && (
-				<MobileMenu
-					handleClick={handleClick}
-					hiddenWrapper={hiddenWrapper}
-					open={open}
-				/>
-			)}
-			<div
-				className={`${styles.layout_container} ${styles.gradient__background}`}
-			>
-				{!open && <NavigationBar handleClick={handleClick} />}
-				<div>{children}</div>
-				<Footer />
-			</div>
+		<div className={styles.container}>
+			<main className={styles.main}>{children}</main>
 		</div>
 	);
 }
