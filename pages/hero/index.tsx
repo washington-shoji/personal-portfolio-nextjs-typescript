1503e42f8bfa769d1ef9b8df48af26f42fa3db85
washington-shoji <shoji.washington@gmail.com>
2022-03-03 22:07:43 +1100

    refactor(page): Refactored the hero page

--- pages/hero/index.tsx
+++ pages/hero/index.tsx
@@ -1,31 +1,11 @@
-import Link from 'next/link';
 import React from 'react';
-import Button from '../../src/components/button/Button';
 import styles from './Hero.module.scss';
 
 export default function Hero(): JSX.Element {
 	return (
 		<div className={styles.container}>
 			<div className={styles.hero}>
-				<p>{`Hi I'm`}</p>
-				<h1>WASHINGTON</h1>
-				<div className={styles.animated__text}>
-					<p>{`A passionate`}</p>
-					<div className={styles.text__wrapper}>
-						<div className={styles.inner__animated__text}>
-							full-stack
-							<br />
-							web developer
-							<br />
-							solving problems
-							<br />
-							with tech.
-						</div>
-					</div>
-				</div>
-				<div className={styles.button}>
-					<Button internalLink={'/skill'} title={'Go to my skills'} />
-				</div>
+				<h1>Washington</h1>
 			</div>
 		</div>
 	);
