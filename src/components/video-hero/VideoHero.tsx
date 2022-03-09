2e62d29c7285970d641a89ae1d3565be7b760d8f
washington-shoji <shoji.washington@gmail.com>
2022-03-09 13:49:07 +1100

    refactor(minor): Minor refactoring

--- src/components/video-hero/VideoHero.tsx
+++ src/components/video-hero/VideoHero.tsx
@@ -5,10 +5,7 @@ export function VideoHero(): JSX.Element {
 	return (
 		<div id={styles.hide} className={styles.video__wrapper}>
 			<video autoPlay loop muted className={styles.main__video}>
-				<source
-					src='https://cdn.videvo.net/videvo_files/video/free/2016-03/large_watermarked/Hologram_Planet_by_nuva_preview.mp4'
-					type='video/mp4'
-				/>
+				<source src='/vid_1.webm' type='video/mp4' />
 			</video>
 			<Hero />
 		</div>
