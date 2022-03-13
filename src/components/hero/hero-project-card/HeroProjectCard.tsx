dfc70d1b0a6a3e8c7a91fcfbd82e09438a9c609f
washington-shoji <shoji.washington@gmail.com>
2022-03-14 10:32:49 +1100

    refactor(minor): Minor refactoring

--- src/components/hero/hero-project-card/HeroProjectCard.tsx
+++ src/components/hero/hero-project-card/HeroProjectCard.tsx
@@ -1,24 +1,41 @@
+import { useMediaQuery } from '../../../utils/hooks/usemediaquery';
 import styles from './HeroProjectCard.module.scss';
 
 export interface IHeroProjectCardProps {
-	title: string;
-	description: string;
 	liveLink: string;
+	desktopImage: string;
+	tabletImage: string;
+	mobileImage: string;
 }
 
 export default function HeroProjectCard(props: IHeroProjectCardProps) {
-	const { title, description, liveLink } = props;
+	const isTable = useMediaQuery('(max-width: 900px)');
+	const isMobile = useMediaQuery('(max-width: 600px)');
+
+	const { liveLink, desktopImage, tabletImage, mobileImage } = props;
 	return (
 		<div className={styles.container}>
-			<div className={styles.card}>
+			<div
+				className={styles.card}
+				style={{
+					backgroundImage: isMobile
+						? `url(${mobileImage})`
+						: isTable
+						? `url(${tabletImage})`
+						: `url(${desktopImage})`,
+					backgroundPosition: 'top',
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
+				}}
+			>
 				<div className={styles.content}>
-					<p>{title}</p>
-					<p>{description}</p>
+					{/* <p>{title}</p>
+					<p>{description}</p> */}
 				</div>
 				<div className={styles.buttons}>
 					<button className={styles.button__style}>
 						<a href={liveLink} target='_blank' rel='noreferrer'>
-							Live
+							View Live
 						</a>
 					</button>
 				</div>
